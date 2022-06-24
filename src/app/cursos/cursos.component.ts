e3d975ff3da1086ede2ce068efa3cf35e4409692
Guilherme Carvalho <97130523+guilhermecarvalho9@users.noreply.github.com>
2022-06-23 23:52:18 -0300

    Criação de componentes de curso não encontrado, query params implementado, paginação e navegação imperativa

--- src/app/cursos/cursos.component.ts
+++ src/app/cursos/cursos.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { CursosService } from './cursos.service';
 
 @Component({
   selector: 'app-cursos',
@@ -6,10 +9,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cursos.component.scss']
 })
 export class CursosComponent implements OnInit {
+  cursos: any[] = [];
+  pagina: number = 0;
 
-  constructor() { }
+  constructor(
+    private cursosService: CursosService,
+    private route: ActivatedRoute,
+    private router: Router
+    ) { }
 
   ngOnInit(): void {
+    this.cursos = this.cursosService.getCursos();
+    this.route.queryParams.subscribe(
+      (queryParams: any) => {
+        this.pagina = queryParams['pagina']
+      }
+    );
+  }
+
+  proximaPagina() {
+    // this.pagina++;
+    this.router.navigate(['/cursos'], {queryParams: {'pagina': ++this.pagina}})
   }
 
 }
